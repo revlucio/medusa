11cc692fc887d2cd14f5a8991e58bc3902485e27
Luke Elliott <revlucio@gmail.com>
2021-12-06 22:12:26 +0000

    Introduce styled components and some sliders

--- src/pages/Calc.js
+++ src/pages/Calc.js
@@ -1,21 +1,44 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+const Page = styled.main`
+  margin: 50px;
+  display: flex;
+  flex-direction: column;
+`;
 
 export const Calc = () => {
   const [amountOfGel, setAmountOfGel] = useState(1);
+  const [solidContent, setSolidContent] = useState(1);
 
   return (
-    <div>
-      <h1>calc</h1>
-      <label>
-        amount per gel (μL):{" "}
-        <input
-          type="range"
-          min="1"
-          max="100"
-          onChange={(e) => setAmountOfGel(e.target.value)}
-        />
-      </label>
-      <span>{amountOfGel} μL</span>
-    </div>
+    <Page>
+      <h1>Gel Calc</h1>
+      <div>
+        <label>
+          amount per gel (μL):{" "}
+          <input
+            type="range"
+            min="1"
+            max="100"
+            onChange={(e) => setAmountOfGel(e.target.value)}
+          />
+        </label>
+        <span>{amountOfGel} μL</span>
+      </div>
+      <div>
+        <label>
+          solid content (%):{" "}
+          <input
+            type="range"
+            min="1"
+            max="10"
+            step="0.1"
+            onChange={(e) => setSolidContent(e.target.value)}
+          />
+        </label>
+        <span>{solidContent}%</span>
+      </div>
+    </Page>
   );
 };
